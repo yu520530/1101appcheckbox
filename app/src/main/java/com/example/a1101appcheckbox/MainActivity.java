9a5e2c7c8b4371895011db96e7a60362f674ed83
yo <ya90312@gmail.com>
2021-11-01 13:52:37 +0800

    新增兩個checkbox並新增事件偵測

--- app/src/main/java/com/example/a1101appcheckbox/MainActivity.java
+++ app/src/main/java/com/example/a1101appcheckbox/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.a1101appcheckbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    CheckBox ck1, ck2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ck1 = (CheckBox) findViewById(R.id.ck1);
+        ck2 = (CheckBox) findViewById(R.id.ck2);
+        ck2.setOnCheckedChangeListener(mylistener);
+        ck1.setOnCheckedChangeListener(mylistener);
     }
+    private CompoundButton.OnCheckedChangeListener mylistener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+        }
+    };
 }
